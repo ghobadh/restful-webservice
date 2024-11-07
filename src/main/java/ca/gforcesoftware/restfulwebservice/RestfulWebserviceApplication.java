e3c538fc84fe0a7d4bac15e8d4c480aeb7a1be35
ghobadh <ghobadh@gmail.com>
2024-11-07 17:05:19 -0500

    Initial commit

--- src/main/java/ca/gforcesoftware/restfulwebservice/RestfulWebserviceApplication.java
+++ src/main/java/ca/gforcesoftware/restfulwebservice/RestfulWebserviceApplication.java
@@ -2,7 +2,9 @@ package ca.gforcesoftware.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+// if you don't want to set up MySQL you just need to an exclusion like this
+// @SpringBootApplication(exclude =  DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class RestfulWebserviceApplication {
 
---
+++ src/main/java/ca/gforcesoftware/restfulwebservice/entity/User.java
@@ -0,0 +1,7 @@
+package ca.gforcesoftware.restfulwebservice.entity;
+
+/**
+ * @author gavinhashemi on 2024-11-07
+ */
+public class User {
+}
