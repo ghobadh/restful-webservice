aeece46b22dd7eb6bf01f39628fb704285eeaa54
ghobadh <ghobadh@gmail.com>
2024-11-07 11:30:06 -0500

    Initial commit

---
+++ src/test/java/ca/gforcesoftware/restfulwebservice/RestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
